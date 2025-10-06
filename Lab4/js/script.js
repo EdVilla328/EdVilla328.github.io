da411d04783a100b0480b5ed17bc779dfef7b5f8
Eduardo Villagrana <villagranaeduardo101@gmail.com>
2025-10-05 23:52:35 -0700

    FInal Lab 4

--- Lab4/js/script.js
+++ Lab4/js/script.js
@@ -2,6 +2,8 @@
 document.querySelector("#zip").addEventListener("change", displayCity);
 document.querySelector("#enteredUser").addEventListener("change", displayUser);
 document.querySelector("#pw").addEventListener("click", displayPassword);
+document.querySelector("#state").addEventListener("change", displayCounty);
+document.querySelector("#sumbitBtn").addEventListener("click", validateForm);
 
 displayStates();
 
@@ -12,10 +14,29 @@ async function displayCity() {
         let response = await fetch(url);
         try {
             let data = await response.json();
-            console.log(data);
-            document.querySelector("#city").textContent = data.city;
-            document.querySelector("#latitude").textContent = data.latitude;
-            document.querySelector("#longitude").textContent = data.longitude;
+
+            if(document.querySelector("#zipFeedback")){
+                document.querySelector("#zipFeedback").textContent = "";
+                document.querySelector("#zipFeedback").style.color = "";
+            }
+
+            if(data && data.city){
+    
+                console.log(data);
+                document.querySelector("#city").textContent = data.city;
+                document.querySelector("#latitude").textContent = data.latitude;
+               document.querySelector("#longitude").textContent = data.longitude;
+            }else{
+                document.querySelector("#city").textContent = "";
+                document.querySelector("#latitude").textContent = "";
+                document.querySelector("#longitude").textContent = "";
+
+                if(document.querySelector("#zipFeedback")){
+                    document.querySelector("#zipFeedback").textContent = "Invalid Zip Code";
+                    document.querySelector("#zipFeedback").style.color = "red";
+                }
+
+            }
         } catch (parseError) {
             console.log("JSON Parsing error " + parseError);
         }
@@ -41,6 +62,8 @@ async function displayStates() {
                 document.querySelector("#state").append(optionElement);
             }
 
+            displayCounty();
+
         } catch (parseError) {
             console.log("JSON Parsing error " + parseError);
         }
@@ -52,22 +75,23 @@ async function displayStates() {
 
 
 async function displayPassword() {
-    let password = document.querySelector("#pass").value;
+    // let password = document.querySelector("#pass").value;
     let url = "https://csumb.space/api/suggestedPassword.php?length=8";
     try {
         let response = await fetch(url);
         try {
             let data = await response.json();
-            let passLn = password.length;
-            if(passLn < 6 ){
-                document.querySelector("#pass").textContent = " Password is too short";
-                document.querySelector("#pass").style.color = "red";
-            }
-            else{
-                document.querySelector("#pass").textContent = " Password is good";
-                document.querySelector("#pass").style.color = "green";
-            }
+            // let passLn = password.length;
+            // if(passLn < 6 ){
+                // document.querySelector("#pass").textContent = " Password is too short";
+                // document.querySelector("#pass").style.color = "red";
+            // }
+            // else{
+                // document.querySelector("#pass").textContent = " Password is good";
+                // document.querySelector("#pass").style.color = "green";
+            // }
             document.querySelector("#pass").textContent = data.password;
+            document.querySelector("#pass").style.color = "blue";
             console.log(data);
         } catch (parseError) {
             console.log("JSON Parsing error " + parseError);
@@ -117,6 +141,17 @@ async function displayCounty() {
             let data = await response.json();
             console.log(data);
 
+            let countySelect = document.querySelector("#county");
+            countySelect.innerHTML = "";
+
+            for (let i of data) {
+                let optionElement = document.createElement("option");
+                optionElement.textContent = i.county;
+                optionElement.value = i.county;
+
+                countySelect.append(optionElement);
+            }
+
         } catch (parseError) {
             console.log("JSON Parsing error " + parseError);
         }
@@ -124,4 +159,28 @@ async function displayCounty() {
         console.log("Network error " + error);
     }
     //alert(zipCode)
+}
+
+function validateForm(){
+    let user = document.querySelector("#enteredUser").value;
+    let pwValue = document.querySelector("#pw").value;
+    let pwConfirmValue = document.querySelector("#pwConfirm").value;
+    let feedbackBox = document.querySelector(".feedback");
+    feedbackBox.innerHTML = "";
+
+    if(user.length <3){
+        feedbackBox.innerHTML += "Username must be at least 3 characters<br>";
+    }
+    if(pwValue.length <6){
+        feedbackBox.innerHTML += "Password must be at least 6 characters<br>";
+    }
+    if(pwValue !== pwConfirmValue){
+        feedbackBox.innerHTML += "Passwords must match<br>";
+    }
+    if(feedbackBox.innerHTML === ""){
+        feedbackBox.innerHTML = "Form is valid! Sending data...";
+        feedbackBox.style.color = "green";
+    }else{
+        feedbackBox.style.color = "red";
+    }
 }
